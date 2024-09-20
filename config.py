1cbf67a9efbb65e51f626e3aa322811a73b187eb
vikas-851 <167636013+vikas-851@users.noreply.github.com>
2024-09-20 21:48:22 +0530

    Update config.py

--- config.py
+++ config.py
@@ -55,7 +55,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 # ----------------------------------------------------------------
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/vikas-851/DAXXMUSIC",
+    "https://github.com/vikas-851/hinata",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "Master")
 GIT_TOKEN = getenv(
